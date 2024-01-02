5dc2d67c8a878b3f9eea99915fd283dc5892656b
coji <coji@techtalk.jp>
2024-01-02 18:10:06 +0900

    Update gitignore, add .tool-versions, and modify VSCode settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
 import remixConfig from './remix.config'
+
+installGlobals()
 
 export default defineConfig({
   plugins: [remix(remixConfig)],
